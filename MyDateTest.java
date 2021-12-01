b920e3f58d449fdd7878897aa72048ce6f1ea838
Zahra <mirzakizahra@gmail.com>
2021-12-01 15:34:00 -0800

    creat method givenValidDateWhenValidDay

--- MyDateTest.java
+++ MyDateTest.java
@@ -13,6 +13,16 @@ public class MyDateTest {
         System.out.println("each test");
         myDate = new MyDate();
     }
+	    @ParameterizedTest
+    @CsvSource({"10,5,32,false","16,7,31,false","13,12,30,false","10,2,-7,false"
+    ,"11,4,20,true"})
+    void givenValidDate_WhenValidDay_ThenAccurateResponseReturn
+            (int year, int month, int day, boolean output) {
+
+        boolean expected = myDate.valid_date(year, month, day);
+        assertEquals(output, expected);
+    }
+
 
 
 
