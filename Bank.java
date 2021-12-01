9d8b1a277662b4aecefbf5ba2af38fd8d626732e
Zahra <mirzakizahra@gmail.com>
2021-12-01 15:20:05 -0800

    add bank class

---
+++ Bank.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Bank {
+    Set<Person>personSet=new HashSet<>();
+    List<Person> fine = new ArrayList<>();
+
+
+}
