95a4afac06e825d4f90de23304ef2ac489495068
Zahra <mirzakizahra@gmail.com>
2021-12-01 15:15:39 -0800

    add Borrow class with it's properties

---
+++ Borrow.java
@@ -0,0 +1,21 @@
+
+
+public class Borrow {
+    private String disk;
+    private MyDate myDate;
+
+    public Borrow(String disk, MyDate myDate) {
+        this.disk = disk;
+        this.myDate = myDate;
+    }
+
+    public String getDisk() {
+        return disk;
+    }
+
+    public MyDate getMyDate() {
+        return myDate;
+    }
+
+
+}
